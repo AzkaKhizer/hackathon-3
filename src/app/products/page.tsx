efdb44ec5558f772d4c880a57327896eafe9f6f8
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2025-01-27 18:16:52 +0500

    page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,6 +5,7 @@ import { client } from "@/sanity/lib/client";
 import { fourProducts } from "@/sanity/lib/queries"; 
 import { Product } from "../../../types/products";
 import Link from "next/link";
+import Image from "next/image";
 import AddToCartButton from "@/components/addtocartbutton";
 
 export default function TopSelling() {
@@ -23,38 +24,37 @@ export default function TopSelling() {
   }, []);
 
   return (
-    <div >
-        
-    <div className="mt-14 w-full h-full max-w-screen-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4  gap-6">
-      
-      {products.length > 0 ? (
-        products.map((product) => (
-          <div
-            key={product._id}
-            className="border border-gray-200 rounded-[20px] p-4 m-2 shadow-sm transition-transform duration-300 ease-in-out hover:scale-110"
-          >
-            <Link href={`/products/${product.slug}`} passHref>
-
-
-            
-            {product.imageUrl && (
-              <img
-                src={product.imageUrl}
-                alt={product.name}
-                className="w-full h-64 object-cover rounded-[20px]  mb-4"
-              />
-            )}
-            <h2 className="text-lg font-bold">{product.name}</h2>
-            <p className="text-gray-700 font-semibold">${product.price}</p>
-            </Link>
-            <AddToCartButton product={product} />
-          </div>
-          
-        ))
-      ) : (
-        <p className="text-center col-span-full">No products available</p>
-      )}
-    </div>
+    <div>
+      <div className="mt-14 w-full h-full max-w-screen-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <div
+              key={product._id}
+              className="border border-gray-200 rounded-[20px] p-4 m-2 shadow-sm transition-transform duration-300 ease-in-out hover:scale-110"
+            >
+              <Link href={`/products/${product.slug}`} passHref>
+                {product.imageUrl && (
+                  <div className="relative w-full h-64 mb-4">
+                    <Image
+                      src={product.imageUrl}
+                      alt={product.name}
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-[20px]"
+                    />
+                  </div>
+                )}
+                <h2 className="text-lg font-bold">{product.name}</h2>
+                <p className="text-gray-700 font-semibold">${product.price}</p>
+              </Link>
+              <AddToCartButton product={product} />
+            </div>
+          ))
+        ) : (
+          <p className="text-center col-span-full">No products available</p>
+        )}
+      </div>
     </div>
   );
 }
+
